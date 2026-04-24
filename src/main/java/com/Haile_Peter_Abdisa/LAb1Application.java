01ef0d5cae9b6568c1e161cc89e66aad2ee882e5
peter <peterkinfe548@gmail.com>
2026-04-24 22:37:17 +0300

    the application entry point

--- src/main/java/com/Haile_Peter_Abdisa/LAb1Application.java
+++ src/main/java/com/Haile_Peter_Abdisa/LAb1Application.java
@@ -4,13 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.servers.Server;
 
 @SpringBootApplication
-@OpenAPIDefinition(
-    info = @Info(title = "Product Service API", version = "1.0.0", description = "RESTful Product Catalogue — Lab 22"),
-    servers = @Server(url = "/", description = "Default Server URL")
-)
+@OpenAPIDefinition(info = @Info(title = "Product Service API", version = "1.0.0", description = "RESTful Product Catalogue — Lab 22"))
 public class LAb1Application {
 
     public static void main(String[] args) {
