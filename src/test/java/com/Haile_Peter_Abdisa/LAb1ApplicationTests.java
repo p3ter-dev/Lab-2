4c1aa3a63de8638f082e5bab76a217a4528c1c05
peter <peterkinfe548@gmail.com>
2026-04-24 20:10:37 +0300

    added baseline spring context load test

---
+++ src/test/java/com/Haile_Peter_Abdisa/LAb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Haile_Peter_Abdisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LAb1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
